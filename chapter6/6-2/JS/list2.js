8fe3a4f1d66cc2cbb21c99e1536e475e1c96cf0f
vagrant <t.barnard.r@gmail.com>
2020-11-25 14:47:37 +0000

    exit 6-2

---
+++ chapter6/6-2/JS/list2.js
@@ -0,0 +1,10 @@
+//HTML,XMLでの文書を操作する　
+//6-7 child_nodes
+var s = document.getElementById('food');
+var opts = s.childNodes;
+for (var i = 0, len = opts.length; i < len; i++) {
+  var opt = opts.item(i);
+  if (opt.nodeType === 1) {
+    console.log(opt.value);
+  }
+}
