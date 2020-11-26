e309bf5c98f96ac441918c76838b8806f3441f8f
vagrant <t.barnard.r@gmail.com>
2020-11-26 06:42:22 +0000

    exit 6-2

--- chapter6/6-2/JS/list2.js
+++ chapter6/6-2/JS/list2.js
@@ -8,3 +8,30 @@ for (var i = 0, len = opts.length; i < len; i++) {
     console.log(opt.value);
   }
 }
+
+//6-8 first_child
+var s = document.getElementById('food');
+var child = s.firstChild;
+while (child) {
+  if (child.nodeType === 1) {
+    console.log(child.value);
+  }
+  child = child.nextSibling;
+}
+
+//6-9 first_child_element
+var s = document.getElementById('food');
+var child = s.firstElementChild;
+while (child) {
+  console.log(child.value);
+  child = child.nextElementSibling;
+}
+
+//6-10 handle
+function btn_click() {
+  window.alert('ボタンがクリックされました。');
+}
+
+
+
+
