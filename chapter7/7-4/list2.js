3a78cceef7f15368e158ac90eae4de45df1272eb
vagrant <t.barnard.r@gmail.com>
2020-12-04 10:29:44 +0000

    edit 7-4

--- chapter7/7-4/list2.js
+++ chapter7/7-4/list2.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
             for (var i = 0; i < bms.length; i++) { 
               var li = document.createElement('li');
               var anchor = document.createElement('a');
-              anchor.href = 'http://b.hatena.ne.jp/' + bms[i].user
+              anchor.href = 'http://test' + bms[i].user
               var text = document.createTextNode(bms[i].user + ' ' + bms[i].comment);
               anchor.appendChild(text);
               li.appendChild(anchor);
@@ -34,5 +34,53 @@ document.addEventListener('DOMContentLoaded', function() {
     xhr.open('GET', 'bm.php?url=' +
       encodeURIComponent(document.getElementById('url').value), true);
     xhr.send(null);
+  }, false);
+}, false);
+//7-27 json
+document.getElementById('btn').addEventListener('click', function() {
+  var url = 'http://test='
+    + encodeURIComponent(document.getElementById('url').value);
+  var scr = document.createElement('script');
+  scr.src = url;
+  document.getElementsByTagName('body').item(0).appendChild(scr);
+}, false);
+function show(data) {
+  if (data === null) {
+    result.textContent = 'ブックマークは存在しませんでした。';
+  } else {
+    var bms = data.bookmarks;
+    var ul = document.createElement('ul');
+    for (var i = 0; i < bms.length; i++) { 
+      var li = document.createElement('li');
+      var anchor = document.createElement('a');
+      anchor.href = 'http://test.ne.jp/' + bms[i].user
+      var text = document.createTextNode(bms[i].user + ' ' + bms[i].comment);
+      anchor.appendChild(text);
+      li.appendChild(anchor);
+      ul.appendChild(li);
+    }
+    result.appendChild(ul);
+  }
+} 
+//7-28 cross_main
+document.addEventListener('DOMContentLoaded', function() {
+  var target = 'http://www.test';
+  document.getElementById('btn').addEventListener('click', function() {
+    document.getElementById('frame').contentWindow.postMessage(
+      document.getElementById('message').value, target);
+  }, false);
+  window.addEventListener('message', function(e) {
+    if (e.origin !== target) { return; }
+    console.log(e.data);
+  }, false);
+}, false);
+//7-29 cross_other
+document.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('message', function(e) {
+    var origin = 'http://localhost';
+    if (e.origin !== origin) { return; }
+    document.getElementById('result1').textContent = e.data;
+    var current = new Date();
+    e.source.postMessage(current, origin);
   }, false);
 }, false);
