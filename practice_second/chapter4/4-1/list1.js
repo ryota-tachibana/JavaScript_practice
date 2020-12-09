b99ad3bbad14fcd2a039e97f976426772c337a31
vagrant <t.barnard.r@gmail.com>
2020-12-09 11:48:48 +0000

    finish 4-1

---
+++ practice_second/chapter4/4-1/list1.js
@@ -0,0 +1,32 @@
+//4-1 function
+function getTriangle(base, height) {
+  return base * height / 2;
+}
+
+console.log(getTriangle(5, 2));
+
+//4-2 function_obj
+var getTriangle = new Function('base', 'height', 'return base * height / 2');
+
+console.log(getTriangle(5, 2));
+
+//4-3 function_obj2
+var param = 'height, width';
+var formula = 'return height * width / 2;';
+var diamond = new Function(param, formula);
+
+console.log('菱形の面積：' + diamond(5, 2));
+
+//4-4 function_literal
+var getTriangle = function(base, height) {
+  return base * height / 2;
+}
+
+console.log(getTriangle(5, 2));
+
+//4-5 arrow
+let getTriang = (base, height) => {
+  return base * height / 2;
+}
+
+console.log(getTriang(5, 2));
