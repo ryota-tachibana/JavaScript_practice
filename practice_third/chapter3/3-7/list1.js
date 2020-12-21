3a8fba9ddb92748f0810554545ad9d8fb3725ac6
vagrant <t.barnard.r@gmail.com>
2020-12-21 13:37:28 +0000

    finish 3-7

---
+++ practice_third/chapter3/3-7/list1.js
@@ -0,0 +1,25 @@
+//3-53 is_nan
+console.log(isNaN('hoge'));
+console.log(Number.isNaN('hoge'));
+
+//3-54 encodeURI
+var str = '!"#$%&()+-*/@~_|;:,.';
+
+console.log(encodeURI(str));
+console.log(encodeURIComponent(str));
+
+//3-55 eval
+var str = 'console.log("eval関数")';
+
+eval(str);
+
+//3-56 eval2
+var obj = {hoge : 1, hoge2 : 2 };
+var prop = 'hoge';
+
+eval('console.log(obj.'+prop+')');
+
+//3-57 json
+var data = JSON.parse('{"hoge": 1, "hoge2": 2}');
+
+console.log(data.hoge);
